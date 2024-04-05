a3f6924ed4969f6367bd3a954b228eccdc1d0b8d
김동환 <forwarder1121@naver.com>
2024-04-05 20:45:54 +0900

    0405

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+a,b=10,20
+
+print("# 교환 전 값")
+print("a : {}, b : {}".format(a,b))
+print("# 교환 전 id a's id:{}, b's id:{}".format(id(a),id(b)))
+print()
+a,b=b,a
+
+print("# 교환 후 id a's id:{}, b's id:{}".format(id(a),id(b)))
